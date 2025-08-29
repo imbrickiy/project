a5aa802ebdae170032736f01be9be44a43cd2bd1
imbrickiy <imbrickiy@ya.ru>
2025-08-29 14:43:31 +0300

    Упрощение конструктора WelcomePage и удаление тестового файла widget_test.dart

--- lib/widgets/welcome.dart
+++ lib/widgets/welcome.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatefulWidget {
-  const WelcomePage({Key? key}) : super(key: key);
+  const WelcomePage({super.key});
 
   @override
   _WelcomePageState createState() => _WelcomePageState();
